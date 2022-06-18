23fb48c1720252a9e51cdcb8e6a941133b3dff66
phmclong <68843407+phmclong@users.noreply.github.com>
2022-06-19 01:20:15 +0700

    deploy

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
+const compression = require('compression');
+const morgan = require('morgan');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config({ path: path.join(__dirname, '../.env') });
@@ -15,9 +18,17 @@ const showtimeRouter = require('./routes/showtime');
 const reservationRouter = require('./routes/reservation');
 const invitationsRouter = require('./routes/invitations');
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
 const app = express();
 app.disable('x-powered-by');
 const port = process.env.PORT || 8080;
+
+app.use(compression());
+app.use(morgan("combined", { stream: accessLogStream }));
 
 // Serve static files from the React app
 // app.use(express.static(path.join(__dirname, '../../client/build')));
@@ -26,21 +37,18 @@ app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
-
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
   // Request headers you wish to allow
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization'
   );
-
   //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
   // Pass to next layer of middleware
   next();
 });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(movieRouter);
@@ -53,7 +61,7 @@ app.use(invitationsRouter);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   // res.sendFile(path.join(__dirname + '../../client/build/index.html'));
   res.status(200).send("Hi express");
 });
